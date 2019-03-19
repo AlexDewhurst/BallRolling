e7c59136966555f319929ee14eedc88345cd230e
AlexDewhurst <dewha618@hsd.k12.or.us>
2019-03-19 10:53:33 -0700

    added following camera and basic walls

--- RollABall/Roll-A-Ball/Assets/PlayerController.cs
+++ RollABall/Roll-A-Ball/Assets/PlayerController.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour{
+
+    public float speed;  
+
+    private Rigidbody rb;
+
     void Update()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
@@ -14,6 +18,8 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        RigidBody
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
     }
 }
